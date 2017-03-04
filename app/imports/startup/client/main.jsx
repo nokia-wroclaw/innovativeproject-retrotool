a641a1c609161428ba7ff951ae009662f7d68e76
Mateusz Stanuch <mateusz.stanuch@gmail.com>
2017-03-04 23:27:30 +0100

    Add material-ui

--- app/imports/startup/client/main.jsx
+++ app/imports/startup/client/main.jsx
@@ -2,13 +2,16 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import routes from './routes.jsx';
 
 const App = () => (
-    <Router history={browserHistory}>
-        {routes}
-    </Router>
+    <MuiThemeProvider>
+        <Router history={browserHistory}>
+            {routes}
+        </Router>
+    </MuiThemeProvider>
 );
 
 Meteor.startup(() => {
