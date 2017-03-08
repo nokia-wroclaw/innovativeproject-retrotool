5dc52ff1aea37874975fedb1f15b24051c496c99
Mateusz Stanuch <mateusz.stanuch@gmail.com>
2017-03-08 21:11:10 +0100

    Add publications

--- app/imports/api/projects/server/publications.js
+++ app/imports/api/projects/server/publications.js
@@ -1,1 +1,44 @@
-// @todo: add publications
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+import { Projects } from './../Projects.js';
+
+const limitQueryToUserProjects = (userId, query) => {
+    const user = Meteor.users.findOne(userId);
+    const { isAdmin } = user;
+    query = isAdmin ? query : query.members = user._id;
+    return query;
+};
+
+Meteor.publish('projectList', function publishProjectList() {
+    if (!this.userId) {
+        return this.ready();
+    }
+    const query = {};
+    limitQueryToUserProjects(this.userId, query);
+
+    const options = {
+        fields: {
+            name: 1,
+        },
+    };
+
+    return Projects.find(query, options);
+});
+
+Meteor.publish('singleProject', function publishSingleProject(projectId) {
+    check(projectId, String);
+
+    if (!this.userId) {
+        return this.ready();
+    }
+
+    const query = {
+        _id: projectId,
+    };
+
+    limitQueryToUserProjects(this.userId, query);
+
+    const options = {};
+
+    return Projects.find(query, options);
+});
