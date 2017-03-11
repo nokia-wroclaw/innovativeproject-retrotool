345762d1fea1535b146d741d071f579eb618d01d
mikze <mikzepro@gmail.com>
2017-03-11 15:32:34 +0100

    Adding admin

---
+++ app/server/makeAdmin.js
@@ -0,0 +1,12 @@
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+
+let num = 0;
+
+Accounts.onCreateUser((options, user) => {
+    console.log('New user created', user.services.github.username, num += 1);
+    if (num === 1) {
+        user.isAdmin = true;
+    }
+    return user;
+});
