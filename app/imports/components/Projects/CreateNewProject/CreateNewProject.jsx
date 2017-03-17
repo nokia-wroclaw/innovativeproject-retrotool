103668c2f42eef15487f1169ca2d519a05de00be
Avogardo <avogardo0@gmail.com>
2017-03-17 18:50:55 +0100

    Add form to creating projects

--- app/imports/components/Projects/CreateNewProject/CreateNewProject.jsx
+++ app/imports/components/Projects/CreateNewProject/CreateNewProject.jsx
@@ -1,4 +1,80 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import { TextField,
+    RaisedButton,
+} from 'material-ui';
+
+class CreateNewProject extends Component {
+
+    constructor(props) {
+        super(props);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.state = {
+            isError: false,
+            isResult: false,
+        };
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const { createNewProject, goToProject } = this.props;
+
+        const name = this.area.input.value;
+        console.log('dziala');
+        const result = createNewProject(name, [0], [0]);
+
+        if (result) {
+            this.setState({
+                isError: false,
+                isResult: true,
+            });
+
+            goToProject(projectId);
+        } else {
+            this.setState({
+                isError: true,
+                isResult: false,
+            });
+        }
+    }
+
+    renderTextField() {
+        const { isError, isResult } = this.state;
+
+        if (!isResult) {
+            return (
+                <TextField
+                    ref={ref => (this.area = ref)}
+                    hintText="New project name"
+                    errorText={isError ? 'This field is required' : ''}
+                />
+            );
+        }
+        return (
+            <span>New project has been created!</span>
+        );
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit}>
+                <h2>Here you can create a new project!</h2>
+                {this.renderTextField()}
+                {!this.state.isError && this.state.isResult ? '' :
+                <RaisedButton label="Add" type="submit" primary />
+                }
+            </form>
+        );
+    }
+}
+
+CreateNewProject.propTypes = {
+    createNewProject: PropTypes.func.isRequired,
+    goToProject: PropTypes.func.isRequired,
+};
+
+export default CreateNewProject;
+
+/*import React from 'react';
 
 const CreateNewProject = () => (
     <h2>Here you can create a new project!</h2>
@@ -7,3 +83,4 @@ const CreateNewProject = () => (
 CreateNewProject.propTypes = {};
 
 export default CreateNewProject;
+*/
--- app/imports/components/Projects/CreateNewProject/index.js
+++ app/imports/components/Projects/CreateNewProject/index.js
@@ -1,3 +1,19 @@
+import { composeWithTracker } from 'react-komposer';
+import { withRouter } from 'react-router';
+
+import { actions as projectActions } from '/imports/api/projects';
+
 import CreateNewProject from './CreateNewProject.jsx';
 
-export default CreateNewProject;
+const composer = (props, onData) => {
+    onData(null, {
+        createNewProject: projectActions.createNewProject,
+        goToProject: projectActions.goToProject,
+    });
+};
+
+export default withRouter(
+      composeWithTracker(
+        composer,
+    )(CreateNewProject),
+);
