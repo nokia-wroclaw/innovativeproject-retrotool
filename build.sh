b64ac22c40bfe2ddb1f5348e5cd5f427290103d7
Mateusz Stanuch <mateusz.stanuch@gmail.com>
2017-03-19 15:38:45 +0100

    Update: meteor build command

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd app
 npm install --production
-meteor build ../meteor-build --architecture os.linux.x86_64
-cd ../meteor-build && tar -zxvf app.tar.gz && cd ..
+meteor build --directory ../meteor-build --architecture os.linux.x86_64
+cd ../meteor-build && cd ..
 docker build -t retrotool .
 rm -rf meteor-build
