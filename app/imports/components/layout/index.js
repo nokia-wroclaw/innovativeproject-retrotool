37e7c41b45083699882c1e7526eb42a7192299d3
Avogardo <avogardo0@gmail.com>
2017-03-21 22:02:59 +0100

    Add logout button

---
+++ app/imports/components/layout/index.js
@@ -0,0 +1,22 @@
+import { Meteor } from 'meteor/meteor';
+import { composeWithTracker } from 'react-komposer';
+import { withRouter, browserHistory } from 'react-router';
+
+import MainLayout from './MainLayout.jsx';
+
+const composer = ({ router }, onData) => {
+
+    const onLogOut = () => Meteor.logout((error) => {
+        browserHistory.push('/login');
+    });
+
+    onData(null, {
+        onLogOut,
+    });
+};
+
+export default withRouter(
+    composeWithTracker(
+        composer,
+    )(MainLayout),
+);
