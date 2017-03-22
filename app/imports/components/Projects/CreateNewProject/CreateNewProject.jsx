8f555e4bd7905b084c42d46712089b9565a7e29d
Avogardo <avogardo0@gmail.com>
2017-03-22 14:00:59 +0100

    full mod name, user names/id in props, error object, no for loops,
    Fix typo
    Change functions names
    Reduce states
    Use forEach instead of for loops

--- app/imports/components/Projects/CreateNewProject/CreateNewProject.jsx
+++ app/imports/components/Projects/CreateNewProject/CreateNewProject.jsx
@@ -11,107 +11,111 @@ class CreateNewProject extends Component {
     constructor(props) {
         super(props);
         this.onSubmit = this.onSubmit.bind(this);
-        this.addMods = this.addMods.bind(this);
+        this.addModerator = this.addModerator.bind(this);
         this.state = {
-            isError: false,
             isResult: false,
-            usersNames: [],
-            usersIds: [],
-            chosenModerators: [],
-            chosenMenbers: [],
+            chosenMods: [],
+            chosenMbrs: [],
             inputError: false,
             memberError: false,
+            error: {
+                type: '',
+                message: '',
+            },
         };
-    }
-
-    componentWillMount() {
-        const users = this.props.userList;
-
-        const names = [];
-        const ids = [];
-        for (let i = 0; i < users.length; i += 1) {
-            names.push(users[i].services.github.username);
-            ids.push(users[i]._id);
-        }
-
-        this.setState({
-            usersNames: names,
-            usersIds: ids,
-        });
     }
 
     onSubmit(e) {
         e.preventDefault();
-        const { createNewProject } = this.props;
-        const { chosenModerators, chosenMenbers, usersIds, usersNames } = this.state;
+        const { createNewProject, userList } = this.props;
+        const { chosenMods, chosenMbrs } = this.state;
 
         const name = this.area.input.value;
 
         const moderatorsId = [];
-        let index = 0;
-        for (let i = 0; i < chosenModerators.length; i += 1) {
-            index = usersNames.indexOf(chosenModerators[i]);
-            moderatorsId.push(usersIds[index]);
-        }
+        let user = '';
 
+        chosenMods.forEach(moderator => {
+            user = userList.find(obj => obj.services.github.username === moderator);
+            moderatorsId.push(user._id);
+            console.log(user);
+        });
+console.log(moderatorsId);
         const membersId = [];
-        index = 0;
-        for (let i = 0; i < chosenMenbers.length; i += 1) {
-            index = usersNames.indexOf(chosenMenbers[i]);
-            membersId.push(usersIds[index]);
-        }
+        chosenMbrs.forEach(member => {
+            user = userList.find(obj => obj.services.github.username === member);
+            membersId.push(user._id);
+        });
 
         const result = createNewProject(name, moderatorsId, membersId);
 
         if (result) {
             this.setState({
-                isError: false,
+                error: {
+                    type: '',
+                    message: '',
+                },
                 isResult: true,
             });
         } else {
             this.setState({
-                isError: true,
+                error: {
+                    type: 'project',
+                    message: 'This field is required',
+                },
                 isResult: false,
             });
         }
     }
 
-    addMods(name) {
-        const usersNames = this.state.usersNames.indexOf(name);
-        const chosenModerators = this.state.chosenModerators.indexOf(name);
+    addModerator(name, names) {
+        const usersNames = names.indexOf(name);
+        const chosenMods = this.state.chosenMods.indexOf(name);
 
-        if (usersNames !== -1 && chosenModerators === -1) {
-            const chosen = this.state.chosenModerators;
+        if (usersNames !== -1 && chosenMods === -1) {
+            const chosen = this.state.chosenMods;
             chosen.push(name);
             this.setState({
-                chosenModerators: chosen,
-                inputError: false,
+                chosenMods: chosen,
+                error: {
+                    type: '',
+                    message: '',
+                },
             });
         } else {
             this.setState({
-                inputError: true,
+                error: {
+                    type: 'moderator',
+                    message: 'There is no user with this name',
+                },
             });
         }
 
-        this.modInput.setState({ searchText: '' });
-        this.modInput.focus();
+        this.moderatorInput.setState({ searchText: '' });
+        this.moderatorInput.focus();
     }
 
-    addMember(name) {
-        const usersNames = this.state.usersNames.indexOf(name);
-        const chosenMenbers = this.state.chosenMenbers.indexOf(name);
-        const chosenModerators = this.state.chosenModerators.indexOf(name);
+    addMember(name, names) {
+        const usersNames = names.indexOf(name);
+        const chosenMbrs = this.state.chosenMbrs.indexOf(name);
+        const chosenMods = this.state.chosenMods.indexOf(name);
 
-        if (usersNames !== -1 && chosenMenbers === -1 && chosenModerators === -1) {
-            const chosen = this.state.chosenMenbers;
+        if (usersNames !== -1 && chosenMbrs === -1 && chosenMods === -1) {
+            const chosen = this.state.chosenMbrs;
             chosen.push(name);
             this.setState({
-                chosenMenbers: chosen,
-                memberError: false,
+                chosenMbrs: chosen,
+                error: {
+                    type: '',
+                    message: '',
+                },
             });
         } else {
             this.setState({
-                memberError: true,
+                error: {
+                    type: 'member',
+                    message: 'There is no user with this name',
+                },
             });
         }
 
@@ -122,23 +126,23 @@ class CreateNewProject extends Component {
     removeRow(e, index, type) {
         e.preventDefault();
 
-        if (type === 'mod') {
-            const moderators = this.state.chosenModerators;
+        if (type === 'moderator') {
+            const moderators = this.state.chosenMods;
             moderators.splice(index, 1);
             this.setState({
-                chosenModerators: moderators,
+                chosenMods: moderators,
             });
         } else {
-            const members = this.state.chosenMenbers;
+            const members = this.state.chosenMbrs;
             members.splice(index, 1);
             this.setState({
-                chosenMenbers: members,
+                chosenMbrs: members,
             });
         }
     }
 
-    renderChosenMembers() {
-        const chosen = this.state.chosenMenbers;
+    renderchosenMbrs() {
+        const chosen = this.state.chosenMbrs;
         return chosen.map((name, index) => <ListItem key={`membername${name}`}>
             {name}
             <RaisedButton onTouchTap={e => this.removeRow(e, index, 'member')} label="Remove" type="submit" />
@@ -146,26 +150,34 @@ class CreateNewProject extends Component {
         );
     }
 
-    renderChosenModerators() {
-        const chosen = this.state.chosenModerators;
+    renderchosenMods() {
+        const chosen = this.state.chosenMods;
         return chosen.map((name, index) => <ListItem key={`modname${name}`}>
             {name}
-            <RaisedButton onTouchTap={e => this.removeRow(e, index, 'mod')} label="Remove" type="submit" />
+            <RaisedButton onTouchTap={e => this.removeRow(e, index, 'moderator')} label="Remove" type="submit" />
         </ListItem>,
         );
     }
 
     renderMemberForm() {
-        const { memberError, isResult, usersNames } = this.state;
+        const { isResult, error } = this.state;
+        const users = this.props.userList;
+
+        const names = [];
+
+        users.forEach(user => {
+            names.push(user.services.github.username);
+        });
+
         if (!isResult) {
             return (
                 <AutoComplete
                     floatingLabelText="Members"
                     filter={AutoComplete.fuzzyFilter}
-                    dataSource={usersNames}
+                    dataSource={names}
                     maxSearchResults={5}
-                    onNewRequest={e => this.addMember(e)}
-                    errorText={memberError ? 'No user find or user is already chosen' : ''}
+                    onNewRequest={e => this.addMember(e, names)}
+                    errorText={error.type === 'member' ? error.message : ''}
                     ref={c => (this.memberInput = c)}
                 />
             );
@@ -174,17 +186,24 @@ class CreateNewProject extends Component {
     }
 
     renderModForm() {
-        const { inputError, isResult, usersNames } = this.state;
+        const { isResult, error } = this.state;
+        const users = this.props.userList;
+
+        const names = [];
+        users.forEach(user => {
+            names.push(user.services.github.username);
+        });
+
         if (!isResult) {
             return (
                 <AutoComplete
                     floatingLabelText="Moderators"
                     filter={AutoComplete.fuzzyFilter}
-                    dataSource={usersNames}
+                    dataSource={names}
                     maxSearchResults={5}
-                    onNewRequest={e => this.addMods(e)}
-                    errorText={inputError ? 'No user find or user is already chosen' : ''}
-                    ref={c => (this.modInput = c)}
+                    onNewRequest={e => this.addModerator(e, names)}
+                    errorText={error.type === 'moderator' ? error.message : ''}
+                    ref={c => (this.moderatorInput = c)}
                 />
             );
         }
@@ -192,13 +211,13 @@ class CreateNewProject extends Component {
     }
 
     renderProjectNameForm() {
-        const { isError, isResult } = this.state;
+        const { error, isResult } = this.state;
         if (!isResult) {
             return (
                 <TextField
                     ref={ref => (this.area = ref)}
                     hintText="New project name"
-                    errorText={isError ? 'This field is required' : ''}
+                    errorText={error.type === 'project' ? error.message : ''}
                 />
             );
         }
@@ -212,16 +231,16 @@ class CreateNewProject extends Component {
             <h2>Here you can create a new project!</h2>
             {this.renderProjectNameForm()}
             {this.renderModForm()}
-            {!this.state.isError && this.state.isResult ? '' :
+            {!this.state.error.type && this.state.isResult ? '' :
             <List>
-                {this.renderChosenModerators()}
+                {this.renderchosenMods()}
             </List>
             }
             {this.renderMemberForm()}
-            {!this.state.isError && this.state.isResult ? '' :
+            {!this.state.error.type && this.state.isResult ? '' :
             <div>
                 <List>
-                    {this.renderChosenMembers()}
+                    {this.renderchosenMbrs()}
                 </List>
                 <RaisedButton onTouchTap={e => this.onSubmit(e)} label="Create project" type="submit" primary />
             </div>
