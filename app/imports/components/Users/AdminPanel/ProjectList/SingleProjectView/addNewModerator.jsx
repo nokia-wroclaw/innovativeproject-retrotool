c13ede0806c17493b31372abc99816984950a98d
mikze <mikzepro@gmail.com>
2017-04-18 23:47:20 +0200

    Some fixes with admin and project management

--- app/imports/components/Users/AdminPanel/ProjectList/SingleProjectView/addNewModerator.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export class AddNewModerator extends React.Component {
-
-    render() {
-        return (<div><h1>ADD MODERATOR</h1>
-        </div>);
-    }
-}
--- app/imports/components/Users/AdminPanel/UsersList/SingleUserView/addNewAdmin.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export class AddNewAdmin extends React.Component {
-
-    render() {
-        return (<div><h1>NEW ADMIN VIEW</h1>
-        </div>);
-    }
-}
