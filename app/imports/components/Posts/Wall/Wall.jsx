2f7816ddcc722f4e38e20496cc874507c4e11b66
Mateusz Stanuch <mateusz.stanuch@gmail.com>
2017-04-21 22:35:47 +0200

    Update: use prop-types package

--- app/imports/components/Posts/Wall/Wall.jsx
+++ app/imports/components/Posts/Wall/Wall.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import AddPost from '../AddPost';
 import WallToolbar from './WallToolbar.jsx';
