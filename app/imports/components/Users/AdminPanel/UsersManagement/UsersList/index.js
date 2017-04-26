a12cb439d8b58aa6db3431f11b93ee8599101886
mikze <mikzepro@gmail.com>
2017-04-26 18:35:30 +0200

    delete useless line in usersList index

--- app/imports/components/Users/AdminPanel/UsersManagement/UsersList/index.js
+++ app/imports/components/Users/AdminPanel/UsersManagement/UsersList/index.js
@@ -4,13 +4,11 @@ import UsersList from './usersList.jsx';
 
 const composer = (props, onData) => {
     const handler = Meteor.subscribe('userData');
-    const callMethod = (nameOfMethod, y, z) => Meteor.call(nameOfMethod, y, z);
 
     if (handler.ready()) {
         const users = Meteor.users.find({});
         onData(null, {
             users,
-            callMethod,
         });
     }
     return 0;
