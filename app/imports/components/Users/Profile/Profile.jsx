abf50336bfadfbbf35b0c31d0036392b3215f63a
Avogardo <avogardo0@gmail.com>
2017-05-13 16:58:55 +0200

    Add card with user content to profile

--- app/imports/components/Users/Profile/Profile.jsx
+++ app/imports/components/Users/Profile/Profile.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {
+    Card,
+    CardActions,
+    CardMedia,
+    CardTitle,
+    FlatButton,
+} from 'material-ui';
 
-const Profile = () =>
-    <div className="content-container">
-        Your Profile
-    </div>
-;
+
+const Profile = ({ user }) => {
+    console.log(user);
+    return (
+        <div className="content-container">
+            <Card className="class">
+                <CardMedia>
+                    <img src={user.profile.avatar} alt="My avatar" />
+                </CardMedia>
+                <CardTitle title={user.profile.name} subtitle={user.isAdmin ? 'Admin' : ''} />
+                <CardActions>
+                    <FlatButton label="Change name" />
+                    <FlatButton label="Connect photo with github" />
+                    <FlatButton label="Connect photo with gravatar" />
+                </CardActions>
+            </Card>
+        </div>
+    );
+};
+
+Profile.propTypes = {
+    user: PropTypes.shape({
+        _id: PropTypes.string.isRequired,
+        isAdmin: PropTypes.bool.isRequired,
+        profile: PropTypes.shape({
+            avatar: PropTypes.string,
+            name: PropTypes.string,
+        }).isRequired,
+    }).isRequired,
+};
 
 export default Profile;
--- app/imports/components/Users/Profile/index.js
+++ app/imports/components/Users/Profile/index.js
@@ -1,3 +1,26 @@
+import { Meteor } from 'meteor/meteor';
+import { composeWithTracker } from 'react-komposer';
+import { withRouter } from 'react-router';
+
+import { FullPageLoader } from '/imports/components/Loaders';
+
 import Profile from './Profile.jsx';
 
-export default Profile;
+const composer = (props, onData) => {
+    const userHandler = Meteor.subscribe('extendedUser');
+
+    if (userHandler.ready()) {
+        const user = Meteor.user();
+
+        onData(null, {
+            user,
+        });
+    }
+};
+
+export default withRouter(
+    composeWithTracker(
+        composer,
+        FullPageLoader,
+    )(Profile),
+);
