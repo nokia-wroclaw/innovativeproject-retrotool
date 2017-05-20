e8bdc0a0921cec51d3e371f67ee81349f031dabc
Mateusz Stanuch <mateusz.stanuch@gmail.com>
2017-05-20 22:46:44 +0200

    Favourite projects (#48)
    * Add markdown to posts and comments
    
    * Add project methods (favourite projects, last viewed)
    
    * Add: redirect to last viewed project on login
    
    * Add: star and unstar projects
    
    * Add: list of favourite projects

--- app/imports/components/Hello/Hello.jsx
+++ app/imports/components/Hello/Hello.jsx
@@ -1,11 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Subheader } from 'material-ui';
+import { List, ListItem } from 'material-ui/List';
 
-const Hello = () => (
+const ProjectsListing = ({ projects, goToProject }) =>
+    <div>
+        <Subheader>Favourite Projects</Subheader>
+        <List>
+            {projects.map(project => (
+                <ListItem
+                    key={project.id}
+                    primaryText={project.name}
+                    onTouchTap={() => goToProject(project.id)}
+                />
+            ))}
+        </List>
+    </div>
+;
+
+ProjectsListing.propTypes = {
+    projects: PropTypes.arrayOf({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+    }).isRequired,
+    goToProject: PropTypes.func.isRequired,
+};
+
+
+const Hello = ({ projects, goToProject }) => (
     <div className="content-container">
-        <h1>Hello!</h1>
+        <h2>Hello!</h2>
+
+        {projects.length ?
+            <ProjectsListing
+                projects={projects}
+                goToProject={goToProject}
+            />
+            :
+            <h4>You do not have favourite projects, star them!</h4>
+        }
     </div>
 );
 
-Hello.propTypes = {};
+Hello.propTypes = {
+    projects: PropTypes.arrayOf({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+    }).isRequired,
+    goToProject: PropTypes.func.isRequired,
+};
+
+Hello.defaultProps = {
+    projects: [],
+    goToProject() {},
+};
 
 export default Hello;
--- app/imports/components/Hello/index.js
+++ app/imports/components/Hello/index.js
@@ -1,3 +1,32 @@
+import { Meteor } from 'meteor/meteor';
+import { composeWithTracker } from 'react-komposer';
+import { Projects, actions } from '/imports/api/projects';
+import { FullPageLoader } from '/imports/components/Loaders';
 import Hello from './Hello.jsx';
 
-export default Hello;
+const composer = ({ params: { projectId, sprintId } }, onData) => {
+    const projectHandler = Meteor.subscribe('projectList');
+    const user = Meteor.user();
+    const { favouriteProjects = [] } = user && user.profile;
+
+    if (projectHandler.ready()) {
+        const projects = Projects.find({
+            _id: { $in: favouriteProjects },
+        }, {
+            fields: { name: 1 },
+        }).map(project => ({
+            name: project.name,
+            id: project._id,
+        }));
+
+        onData(null, {
+            projects,
+            goToProject: actions.goToProject,
+        });
+    }
+};
+
+export default composeWithTracker(
+    composer,
+    FullPageLoader,
+)(Hello);
