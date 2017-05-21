920138b9de595b8a18d58c80207f4e59a90f7625
Avogardo <avogardo0@gmail.com>
2017-05-21 17:18:31 +0200

    Add sort rules

--- app/imports/components/WorkingAgreements/WorkingAgreementBoard/utils.js
+++ app/imports/components/WorkingAgreements/WorkingAgreementBoard/utils.js
@@ -9,21 +9,15 @@ const sorts = {
     createDateNewestToOldest({ createdAt: a }, { createdAt: b }) {
         return moment(a).isBefore(b);
     },
-    startDateOldestToNewest({ startDate: a }, { startDate: b }) {
+    validityDateOldestToNewest({ date: a }, { date: b }) {
         return moment(a).isAfter(b);
     },
-    startDateNewestToOldest({ startDate: a }, { startDate: b }) {
-        return moment(a).isBefore(b);
-    },
-    latestDeadline({ endDate: a }, { endDate: b }) {
-        return moment(a).isAfter(b);
-    },
-    earliestDeadline({ endDate: a }, { endDate: b }) {
+    validityDateNewestToOldest({ date: a }, { date: b }) {
         return moment(a).isBefore(b);
     },
 };
 
-export const getDefaultOptionValue = () => 'earliestDeadline';
+export const getDefaultOptionValue = () => 'validityDateNewestToOldest';
 
 export const sortOptions = _.keys(sorts)
     .map(option =>
