1a00f308224cd8b67e12961d09aaec9f5f8c5ec4
Avogardo <avogardo0@gmail.com>
2017-05-24 18:16:38 +0200

    add props

--- app/imports/components/CustomFormFields/CustomDatePicker.jsx
+++ app/imports/components/CustomFormFields/CustomDatePicker.jsx
@@ -11,7 +11,16 @@ const CustomDatePicker = props => (
         hintText={props.hintText}
         floatingLabelText={props.floatingLabelText}
         minDate={props.min}
+        maxDate={props.maxDate}
         defaultDate={props.defaultDate}
+        autoOk={props.autoOk}
+        className={props.className}
+        container={props.container}
+        disableYearSelection={props.disableYearSelection}
+        disabled={props.disabled}
+        firstDayOfWeek={props.firstDayOfWeek}
+        hideCalendarDate={props.hideCalendarDate}
+        mode={props.mode}
     />
 );
 
@@ -19,8 +28,17 @@ CustomDatePicker.defaultProps = {
     name: '',
     hintText: '',
     floatingLabelText: '',
+    className: '',
+    container: 'dialog',
+    mode: 'portrait',
     min: new Date(2000, 1, 1),
-    defaultDate: {},
+    maxDate: new Date(2100, 1, 1),
+    defaultDate: new Date(),
+    autoOk: false,
+    disableYearSelection: false,
+    disabled: false,
+    hideCalendarDate: false,
+    firstDayOfWeek: 1,
 };
 
 CustomDatePicker.propTypes = {
@@ -28,8 +46,17 @@ CustomDatePicker.propTypes = {
     onChange: PropTypes.func.isRequired,
     hintText: PropTypes.string,
     min: PropTypes.instanceOf(Date),
+    maxDate: PropTypes.instanceOf(Date),
     defaultDate: PropTypes.instanceOf(Date),
     floatingLabelText: PropTypes.string,
+    className: PropTypes.string,
+    container: PropTypes.string,
+    mode: PropTypes.string,
+    autoOk: PropTypes.bool,
+    disableYearSelection: PropTypes.bool,
+    disabled: PropTypes.bool,
+    hideCalendarDate: PropTypes.bool,
+    firstDayOfWeek: PropTypes.number,
 };
 
 export default connectField(CustomDatePicker, { ensureValue: false });
