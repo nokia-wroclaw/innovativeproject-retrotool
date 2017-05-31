ef35dcf5a6c81924c84acc0724794281efb8247d
Mateusz Stanuch <mateusz.stanuch@gmail.com>
2017-05-31 22:49:13 +0200

    Update: publications, add category manager component

--- app/imports/components/CategoryManager/CategoryManager.jsx
+++ app/imports/components/CategoryManager/CategoryManager.jsx
@@ -1,17 +1,66 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+    Card,
+    CardActions,
+    CardTitle,
+    CardText,
+    Chip,
+    FlatButton,
+} from 'material-ui';
 
-class CategoriesManager extends React.Component {
+const CategoriesManager = ({
+    projectId,
+    projectCategories,
+    globalCategories,
+    canEditGlobalCategories,
+    canEditProjectCategories,
+}) =>
+    <div>
+        Project Manager
+
+        <Card>
+            <CardTitle
+                title="Global Categories"
+            />
+            <CardText>
+                {globalCategories.map(category =>
+                    <Chip key={category.value}>{category.label}</Chip>,
+                )}
+            </CardText>
+            {canEditGlobalCategories &&
+                <CardActions>
+                    <FlatButton
+                        label="Add category"
+                        onTouchTap={() => {}}
+                    />
+                </CardActions>
+            }
+        </Card>
 
 
-    render() {
-        return (
-            <div>
-                Project Manager...
-            </div>
-        );
-    }
-}
+        {projectId &&
+            <Card>
+                <CardTitle
+                    title="Project Categories"
+                />
+                <CardText>
+                    {projectCategories.map(category =>
+                        <Chip className="chip-style" key={category.value}>{category.label}</Chip>,
+                    )}
+                </CardText>
+                {canEditProjectCategories &&
+                    <CardActions>
+                        <FlatButton
+                            label="Add category"
+                            onTouchTap={() => {}}
+                        />
+                    </CardActions>
+                }
+            </Card>
+        }
+    </div>
+;
 
 const categoryPropTypes = PropTypes.shape({
     label: PropTypes.string.isRequired,
@@ -19,13 +68,19 @@ const categoryPropTypes = PropTypes.shape({
 }).isRequired;
 
 CategoriesManager.propTypes = {
+    projectId: PropTypes.string,
+    canEditGlobalCategories: PropTypes.bool.isRequired,
+    canEditProjectCategories: PropTypes.bool.isRequired,
     globalCategories: PropTypes.arrayOf(categoryPropTypes).isRequired,
     projectCategories: PropTypes.arrayOf(categoryPropTypes).isRequired,
 };
 
 CategoriesManager.defaultProps = {
+    projectId: '',
     globalCategories: [],
     projectCategories: [],
+    canEditGlobalCategories: false,
+    canEditProjectCategories: false,
 };
 
 export default CategoriesManager;
--- app/imports/components/CategoryManager/index.js
+++ app/imports/components/CategoryManager/index.js
@@ -1,3 +1,42 @@
+import { Meteor } from 'meteor/meteor';
+import { composeWithTracker } from 'react-komposer';
+import { withRouter } from 'react-router';
+import { Categories } from '/imports/api/categories';
+import { isAdmin } from '/imports/api/users';
+import { isProjectModerator } from '/imports/api/projects';
 import CategoryManager from './CategoryManager.jsx';
 
-export default CategoryManager;
+const composer = ({ params: { projectId = '' } }, onData) => {
+    const categoriesHandler = Meteor.subscribe('categories', projectId);
+
+    const userId = Meteor.userId();
+    const canEditGlobalCategories = isAdmin();
+    const canEditProjectCategories = projectId && isProjectModerator(projectId, userId);
+
+    if (categoriesHandler.ready()) {
+        const projectCategories = Categories.find({ projectId }).map(
+            category => ({
+                value: category._id,
+                label: category.name,
+            }),
+        );
+        const globalCategories = Categories.find({ projectId: { $exists: false } }).map(
+            category => ({
+                value: category._id,
+                label: category.name,
+            }),
+        );
+
+        onData(null, {
+            projectId,
+            canEditGlobalCategories,
+            canEditProjectCategories,
+            projectCategories,
+            globalCategories,
+        });
+    }
+};
+
+export default withRouter(
+    composeWithTracker(composer)(CategoryManager),
+);
