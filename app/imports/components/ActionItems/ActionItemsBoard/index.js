25cc1d99567fdd9e5e59d45322b17e708c2be00b
Jakub Wolny <avogardo0@gmail.com>
2017-06-15 19:27:40 +0200

    Refactor action items component (#60)
    no mix meteor and composer with react
    simplify composer
    move ai actions functions to aiBoard component

--- app/imports/components/ActionItems/ActionItemsBoard/index.js
+++ app/imports/components/ActionItems/ActionItemsBoard/index.js
@@ -13,64 +13,21 @@ import { Sprints } from '/imports/api/sprints';
 
 import ActionItems from './ActionItemsBoard.jsx';
 
+const composer = ({ params: { projectId, sprintId }, hideButton }, onData) => {
+    const handlers = [
+        Meteor.subscribe('actionItems', sprintId || projectId, !!sprintId),
+        Meteor.subscribe('singleProject', projectId),
+        Meteor.subscribe('projectMembers', projectId),
+    ];
 
-const toggleActionItem = (
-    actionItemId,
-    closeMessage,
-    onData,
-    sprintId,
-    projectId,
-    handlers,
-    hideButton,
-    wrappedData,
-) => {
-    actionItemsActions.toggleActionItemState(
-        actionItemId,
-        closeMessage,
-    ).then((toggleResult) => {
-        wrappedData(onData, sprintId, projectId, handlers, hideButton, {
-            toggleResult: !!toggleResult,
-        });
-    }).catch((error) => {
-        wrappedData(onData, sprintId, projectId, handlers, hideButton, {
-            errorToggle: error,
-        });
-    });
-};
+    if (sprintId) {
+        handlers.push(Meteor.subscribe('singleSprint', sprintId));
+    }
 
-const addActionItem = (
-    sprintId,
-    projectId,
-    startDate,
-    endDate,
-    assigneeId,
-    text,
-    onData,
-    handlers,
-    hideButton,
-    wrappedData,
-) => {
-    actionItemsActions.createActionItem(
-        sprintId,
-        startDate,
-        endDate,
-        assigneeId,
-        text,
-    ).then((addResult) => {
-        wrappedData(onData, sprintId, projectId, handlers, hideButton, {
-            addResult: !!addResult,
-        });
-    }).catch((error) => {
-        wrappedData(onData, sprintId, projectId, handlers, hideButton, {
-            errorAdd: error,
-        });
-    });
-};
-
-const wrappedData = (onData, sprintId, projectId, handlers, hideButton, data) => {
     if (handlers.every(handler => handler.ready())) {
-        const userId = Meteor.userId();
         const sprint = Sprints.findOne(sprintId);
+        const isClosed = sprint ? sprint.closed : true;
+        const userId = Meteor.userId();
         const isMember = isProjectMember(projectId, userId);
         const isModerator = isProjectModerator(projectId, userId);
         const actionItems = ActionItemsCollection.find().fetch();
@@ -86,39 +43,17 @@ const wrappedData = (onData, sprintId, projectId, handlers, hideButton, data) =>
             return false;
         });
 
-        const isClosed = sprint ? sprint.closed : true;
-
         onData(null, {
-            onData,
-            handlers,
             hideButton,
-            wrappedData,
             actionItems,
-            toggleActionItem,
-            addActionItem,
+            toggleActionItemState: actionItemsActions.toggleActionItemState,
+            createActionItem: actionItemsActions.createActionItem,
             isMember,
             isModerator,
-            sprintId,
-            projectId,
+            sprintId: sprintId || '',
             isClosed,
             userId,
-            ...data,
         });
-    }
-};
-
-const composer = ({ params: { projectId, sprintId }, hideButton }, onData) => {
-    const handlers = [
-        Meteor.subscribe('actionItems', sprintId || projectId, !!sprintId),
-        Meteor.subscribe('singleProject', projectId),
-        Meteor.subscribe('projectMembers', projectId),
-    ];
-
-    if (sprintId) {
-        handlers.push(Meteor.subscribe('singleSprint', sprintId));
-        wrappedData(onData, sprintId, projectId, handlers, hideButton);
-    } else {
-        wrappedData(onData, '', projectId, handlers, hideButton);
     }
 };
 
